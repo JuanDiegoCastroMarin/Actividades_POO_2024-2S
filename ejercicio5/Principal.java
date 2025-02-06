50cb8fa40a6af4770aee946bdea6c2319285e3dd
JuanDiegoCastroMarin <jucastroma@unal.edu.co>
2025-02-06 17:14:17 -0500

    ejercicio5

---
+++ ejercicio5/Principal.java
@@ -0,0 +1,9 @@
+package ejercicio5;
+
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal = new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+    }
+}   
